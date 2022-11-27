140e00a97bda0fc4bc187fdbd6b3615e342cd89c
Said Al Fakhri <alfakhri1998@gmail.com>
2022-11-27 11:53:50 +0700

    fix: update profile model

--- lib/data/models/profile_model.dart
+++ lib/data/models/profile_model.dart
@@ -18,61 +18,37 @@ class ProfileModel {
 
 class User {
   int? id;
-  String? email;
   String? name;
-  int? role;
-  String? address;
-  String? phoneNumber;
-  String? photo;
-  String? token;
+  String? email;
+  String? emailVerifiedAt;
   String? createdAt;
   String? updatedAt;
-  String? emailVerifiedAt;
-  String? photoUrl;
 
   User(
       {this.id,
-      this.email,
       this.name,
-      this.role,
-      this.address,
-      this.phoneNumber,
-      this.photo,
-      this.token,
-      this.createdAt,
-      this.updatedAt,
+      this.email,
       this.emailVerifiedAt,
-      this.photoUrl});
+      this.createdAt,
+      this.updatedAt});
 
   User.fromJson(Map<String, dynamic> json) {
     id = json['id'];
-    email = json['email'];
     name = json['name'];
-    role = json['role'];
-    address = json['address'];
-    phoneNumber = json['phone_number'];
-    photo = json['photo'];
-    token = json['token'];
+    email = json['email'];
+    emailVerifiedAt = json['email_verified_at'];
     createdAt = json['created_at'];
     updatedAt = json['updated_at'];
-    emailVerifiedAt = json['email_verified_at'];
-    photoUrl = json['photo_url'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['id'] = this.id;
-    data['email'] = this.email;
     data['name'] = this.name;
-    data['role'] = this.role;
-    data['address'] = this.address;
-    data['phone_number'] = this.phoneNumber;
-    data['photo'] = this.photo;
-    data['token'] = this.token;
+    data['email'] = this.email;
+    data['email_verified_at'] = this.emailVerifiedAt;
     data['created_at'] = this.createdAt;
     data['updated_at'] = this.updatedAt;
-    data['email_verified_at'] = this.emailVerifiedAt;
-    data['photo_url'] = this.photoUrl;
     return data;
   }
 }
